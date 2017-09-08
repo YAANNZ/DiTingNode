251ee3c094595528cda1dc89efd512886f9590f5
ZYYNN <zhuynchn@163.com>
2017-09-08 17:52:49 +0800

    add Mongodb

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var http = require("http");
+var MongoClient = require('mongodb').MongoClient
+   , assert = require('assert');
+var dburl = 'mongodb://localhost:27017/diting';
+
+MongoClient.connect(dburl, function(err, db){
+	assert.equal(null, err);
+	console.log("Connected succesfully to server");
+
+	db.close();
+});
 
 var server = http.createServer(function(req, res){
 	res.setHeader("Content-Type", "text/html; charset=UTf-8");
 	res.end("Hello");
 });
 
-server.listen(3000, "127.0.0.1");
+// server.listen(3000, "127.0.0.1");
